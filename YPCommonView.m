149e93a0aef866380cca3b0cf2364cd1d45e0051
yanpei <yanpei@haodf.com>
2016-06-29 16:25:32 +0800

    yellow

--- YPCommonView.m
+++ YPCommonView.m
@@ -14,7 +14,7 @@
 {
     self = [super init];
     if (self) {
-        self.backgroundColor = [UIColor blueColor];
+        self.backgroundColor = [UIColor yellowColor];
     }
     return self;
 }
