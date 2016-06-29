0087c737dc98f6a53ab548dca580af11986b0860
yanpei <yanpei@haodf.com>
2016-06-29 14:09:55 +0800

    newSub

---
+++ YPCommonView.h
@@ -0,0 +1,13 @@
+//
+//  YPCommonView.h
+//  PrjA
+//
+//  Created by 闫培 on 16/6/29.
+//  Copyright © 2016年 haodf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YPCommonView : UIView
+
+@end
---
+++ YPCommonView.m
@@ -0,0 +1,22 @@
+//
+//  YPCommonView.m
+//  PrjA
+//
+//  Created by 闫培 on 16/6/29.
+//  Copyright © 2016年 haodf. All rights reserved.
+//
+
+#import "YPCommonView.h"
+
+@implementation YPCommonView
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.backgroundColor = [UIColor redColor];
+    }
+    return self;
+}
+
+@end
